5ee890a63f9d254cbc3a6e586996d6a03b7a8c61
BenDunjay <bendunjay@outlook.com>
2020-07-15 22:46:35 +0100

    playing with doubles and instance doubles

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,16 +1,16 @@
 require "rails_helper"
 
 RSpec.describe Pet, type: :model do
-  let(:owner) { double(user: "Mr.Owner", pet_animal: true) }
-  subject { described_class.new(user: owner) }
-  describe "Associations" do
-    it { should belong_to(:user).without_validating_presence }
-  end
-
   describe "#be_happy method" do
+    let(:owner) { double(User, :id => 2, :name => "Foo", :email => "foo@foo", :password => "Foo", pet_animal: true) }
+    subject { described_class.new(name: "Petty", user_id: owner.id) }
     it "expects an owner to pet it" do
       expect(owner).to receive(:pet_animal)
       subject.be_happy
     end
+  end
+
+  describe "Associations" do
+    it { should belong_to(:user).without_validating_presence }
   end
 end
